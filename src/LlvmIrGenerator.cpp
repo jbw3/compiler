53f568b1037496b13fb17b99e626403d7af86f7f
John Wilkes <jwilkes0011@gmail.com>
2019-03-21 21:28:34 -0500

    Generating a native object file

--- src/LlvmIrGenerator.cpp
+++ src/LlvmIrGenerator.cpp
@@ -2,8 +2,14 @@
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #include "LlvmIrGenerator.h"
 #include "SyntaxTree.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
 #include "utils.h"
 #include <iostream>
 #pragma clang diagnostic pop
@@ -131,6 +137,49 @@ bool LlvmIrGenerator::GenerateCode(const SyntaxTreeNode* syntaxTree)
         return false;
     }
 
+    string targetTripple = sys::getDefaultTargetTriple();
+    InitializeAllTargetInfos();
+    InitializeAllTargets();
+    InitializeAllTargetMCs();
+    InitializeAllAsmParsers();
+    InitializeAllAsmPrinters();
+
+    string errorMsg;
+    const Target* target = TargetRegistry::lookupTarget(targetTripple, errorMsg);
+    if (target == nullptr)
+    {
+        cerr << errorMsg;
+        return false;
+    }
+
+    TargetOptions options;
+    auto relocModel = Optional<Reloc::Model>();
+    TargetMachine* targetMachine =
+        target->createTargetMachine(targetTripple, "generic", "", options, relocModel);
+
+    module.setDataLayout(targetMachine->createDataLayout());
+    module.setTargetTriple(targetTripple);
+
+    string filename = "output.o";
+    error_code ec;
+    raw_fd_ostream outFile(filename, ec, sys::fs::F_None);
+    if (ec)
+    {
+        cerr << ec.message();
+        return false;
+    }
+
+    legacy::PassManager passManager;
+    TargetMachine::CodeGenFileType fileType = TargetMachine::CGFT_ObjectFile;
+    if (targetMachine->addPassesToEmitFile(passManager, outFile, nullptr, fileType))
+    {
+        cerr << "Target machine cannot emit a file of this type\n";
+        return false;
+    }
+
+    passManager.run(module);
+
+    // print LLVM IR
     module.print(outs(), nullptr);
 
     return true;
