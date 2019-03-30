37103f8e21345bb2379e62e866bbbd83fc988609
John Wilkes <jwilkes0011@gmail.com>
2019-03-30 12:12:03 -0500

    Improving unit test

--- src/unittests/LexicalAnalyzerTests.cpp
+++ src/unittests/LexicalAnalyzerTests.cpp
@@ -16,15 +16,44 @@ bool LexicalAnalyzerTests::Run()
 bool LexicalAnalyzerTests::TestValidInputs()
 {
     stringstream ss;
-    vector<Token> tokens;
+    vector<Token> expectedTokens;
+    vector<Token> actualTokens;
 
     LexicalAnalyzer analyzer;
 
-    ss.str("fun test() 1 + 2");
-    bool ok = analyzer.Process(ss, tokens);
-    if (!ok)
+    ss.str("fun test() 1 + 2\n");
+    expectedTokens = {Token("fun", 1, 1), Token("test", 1, 5), Token("(", 1, 9),
+                      Token(")", 1, 10),  Token("1", 1, 12),   Token("+", 1, 14),
+                      Token("2", 1, 16),  Token("\n", 1, 17)};
+    bool ok = analyzer.Process(ss, actualTokens);
+    if (ok)
+    {
+        ok = TokenSequencesAreEqual(expectedTokens, actualTokens);
+    }
+
+    return ok;
+}
+
+bool LexicalAnalyzerTests::TokensAreEqual(const Token& token1, const Token& token2)
+{
+    return token1.GetValue() == token2.GetValue();
+}
+
+bool LexicalAnalyzerTests::TokenSequencesAreEqual(const std::vector<Token>& sequence1,
+                                                  const std::vector<Token>& sequence2)
+{
+    if (sequence1.size() != sequence2.size())
     {
         return false;
+    }
+
+    for (size_t i = 0; i < sequence1.size(); ++i)
+    {
+        bool areEqual = TokensAreEqual(sequence1[i], sequence2[i]);
+        if (!areEqual)
+        {
+            return false;
+        }
     }
 
     return true;
--- src/unittests/LexicalAnalyzerTests.h
+++ src/unittests/LexicalAnalyzerTests.h
@@ -1,6 +1,9 @@
 #ifndef LEXICAL_ANALYZER_TESTS_H_
 #define LEXICAL_ANALYZER_TESTS_H_
 
+#include "Token.h"
+#include <vector>
+
 class LexicalAnalyzerTests
 {
 public:
@@ -8,6 +11,11 @@ public:
 
 private:
     bool TestValidInputs();
+
+    bool TokensAreEqual(const Token& token1, const Token& token2);
+
+    bool TokenSequencesAreEqual(const std::vector<Token>& sequence1,
+                                const std::vector<Token>& sequence2);
 };
 
 #endif // LEXICAL_ANALYZER_TESTS_H_
