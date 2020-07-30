c315be57d377ba4f61f116ca37a4f302f773e053
John Wilkes <jwilkes0011@gmail.com>
2020-07-29 22:02:11 -0500

    Fixing a test on Windows

--- src/unittests/CompilerTests.cpp
+++ src/unittests/CompilerTests.cpp
@@ -8,8 +8,13 @@ using namespace std;
 
 const unordered_map<string, string> lineMap =
 {
+#ifdef _WIN32
+    { "target_datalayout", "target datalayout = \"e-m:w-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"" },
+    { "target_triple", "target triple = \"x86_64-pc-windows-msvc\"" },
+#else
     { "target_datalayout", "target datalayout = \"e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"" },
     { "target_triple", "target triple = \"x86_64-pc-linux-gnu\"" },
+#endif
 };
 
 CompilerTests::CompilerTests()
