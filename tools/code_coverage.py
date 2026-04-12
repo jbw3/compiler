a2568294d6df1961c9da6b5699630d2581fb6a75
John Wilkes <jwilkes0011@gmail.com>
2026-04-12 13:13:10 -0500

    Adding code coverage script

---
+++ tools/code_coverage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import subprocess
+
+def main() -> None:
+    script_dir = Path(__file__).parent
+    root_dir = script_dir.parent
+    build_dir = root_dir / 'debug'
+
+    # run ctest
+    subprocess.check_call(
+        ['ctest', '--output-on-failure'],
+        env={'LLVM_PROFILE_FILE': 'default.profraw'},
+        cwd=build_dir,
+    )
+
+    prof_raw_path = root_dir / 'default.profraw'
+
+    subprocess.check_call(
+        ['llvm-profdata-20', 'merge', '-sparse', prof_raw_path, '-o', 'default.profdata'],
+        cwd=build_dir,
+    )
+
+    subprocess.check_call(
+        ['llvm-cov-20', 'show', '--instr-profile', 'default.profdata', 'unittests/unittests', '-format=html', '-output-dir=../coverage-report'],
+        cwd=build_dir,
+    )
+
+    with open(build_dir / 'lcov.info', 'wb') as lcov_info:
+        subprocess.check_call(
+            ['llvm-cov-20', 'export', '--instr-profile', 'default.profdata', 'unittests/unittests', '-format=lcov'],
+            stdout=lcov_info,
+            cwd=build_dir,
+        )
+
+if __name__ == '__main__':
+    main()
